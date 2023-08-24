f1842ffc0fbd97fe6127ea6b508778300cda3905
sergio-g-v <sergio.gonzalez-v@mail.escuelaing.edu.co>
2023-08-24 08:25:09 -0500

    1.2 punto

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 9999999);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 19999999);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);
 
-		
-		//p1
+		pft1.start();
+		pft2.start();
+		pft3.start();
+
 	}
 	
 }
